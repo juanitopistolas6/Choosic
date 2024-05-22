467127041c2e20bbd7f080f4b871c340a042757a
ikarepli <chaqui@live.com.mx>
2024-05-21 22:57:12 -0700

    agregar canciones en tiepo real

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -5,7 +5,7 @@ import mysql from 'mysql2/promise';
 const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '6442340710juan1',
+  password: '12345',
   database: 'choosic'
 });
 
